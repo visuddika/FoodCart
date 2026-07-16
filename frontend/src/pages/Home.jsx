9df996c89a7e8f9b5afb5b853ffc8cde3e7f28c4
visuddika Chathurani <udumullagevisuddika@gmail.com>
2026-07-16 10:52:57 +0530

    Frontend new update files

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ItemsHome from '../components/ItemsHome';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <ItemsHome />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Items.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Item from '../components/Item';
+
+const Items = () => {
+  return (
+    <div>
+      <Navbar />
+      <Item />
+      <Footer />
+    </div>
+  );
+};
+
+export default Items;
